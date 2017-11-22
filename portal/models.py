d370dd08dd63a737f5b05446ca3760f9ee5bd623
shardul <shardul.negi21@gmail.com>
2017-11-23 01:24:13 +0530

    updated models

--- portal/models.py
+++ portal/models.py
@@ -5,6 +5,9 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.name
+
 
 class Student(models.Model):
     stud_id = models.CharField(max_length=10)
@@ -14,12 +17,18 @@ class Student(models.Model):
     year = models.CharField(max_length=20)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.first_name
+
 
 class Guide(models.Model):
     staff_id = models.CharField(max_length=10)
     access_level = models.BooleanField(default=False)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.first_name
 
 
 class ProjectGuide(models.Model):
