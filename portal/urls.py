7770f96e8462b805c7a94e9ed893c03c0cb0e074
Akshay Pai <sadypai@gmail.com>
2017-11-23 10:18:38 +0530

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^students/', views.students),
     url(r'^projects/', views.projects),
+    url(r'^guides/', views.guides),
+    url(r'^projectguide', views.projectguide),
+    url(r'^projects/', views.projects),
 ]
