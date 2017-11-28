8deb0153d75515317fbf55513e670207ff32614b
shardul <shardul.negi21@gmail.com>
2017-11-29 02:15:45 +0530

    try1

--- portal/views.py
+++ portal/views.py
@@ -56,7 +56,7 @@ def studentsPage(request):
 
 @login_required
 def students(request):
-    return HttpResponse(serializers.serialize("json", UserInfo.objects.filter()))
+    return HttpResponse(serializers.serialize("json", UserInfo.objects.filter(usertype = 's')))
 
 
 @login_required
