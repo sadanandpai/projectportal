ffa152e6401957597443cab9c2a8c343b963e523
Akshay Pai <sadypai@gmail.com>
2017-11-29 12:18:53 +0530

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -56,7 +56,7 @@ def studentsPage(request):
 
 @login_required
 def students(request):
-    return HttpResponse(serializers.serialize("json", UserInfo.objects.select_related('user').all()))
+    return HttpResponse(serializers.serialize("json", UserInfo.objects.select_related('user__username').all()))
 
 
 @login_required
