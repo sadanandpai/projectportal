a290002dad0e472ea57431e2f7eda70f5277a574
Akshay Pai <sadypai@gmail.com>
2017-11-30 14:51:32 +0530

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -8,12 +8,13 @@ urlpatterns = [
     url(r'^signout/$', views.signout),
 
     url(r'^students/$', views.studentsProfile),
+    url(r'^students/(?P<username>[A-z0-9]+)/$', views.students),
     url(r'^students/changePassword/$', views.studentChangePassword),
 
     url(r'^guide/students/$', views.students),
     url(r'^guide/students/crud/$', views.studentsPage),
     url(r'^guide/students/add/$', views.addStudents),
-    url(r'^guide/students/delete/(?P<username>[0-9]+)/$', views.deleteStudents),
+    url(r'^guide/students/delete/(?P<username>[A-z0-9]+)/$', views.deleteStudents),
     url(r'^guide/students/update/$', views.updateStudents),
     url(r'^guide/students/(?P<username>[A-z0-9]+)/$', views.viewstudentsProfile),
 
